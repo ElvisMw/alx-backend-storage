656603c0bca6c7ab42544a1d17e2240672dad660
ElvisMw <elvismwanthi@gmail.com>
2024-05-09 18:39:45 +0300

    11 need meeting

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,9 +1,6 @@
 -- 0x00-MySQL_Advanced\11-need_meeting.sql
 -- SQL script that creates a view need_meeting that lists all students that
--- have a score under 80 (strict) and no last_meeting or more than 1 month.
--- Requirements: The view need_meeting should return all students name when:
--- They score are under (strict) to 80
--- AND no last_meeting date OR more than a month
+-- have a score under 80 (strict) and no last_meeting or more than 1 month
 
 DELIMITER $$
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
