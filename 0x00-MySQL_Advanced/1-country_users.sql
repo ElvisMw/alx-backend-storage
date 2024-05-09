56316ba101cbbbfc40124b66c2562e891aa090b8
ElvisMw <elvismwanthi@gmail.com>
2024-05-09 16:57:01 +0300

    task 1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- Select users with emails containing "gmail.com" but not "yahoo.com"
+SELECT *
+FROM users
+-- Emails containing "gmail.com"
+WHERE email LIKE '%gmail.com' 
+-- Emails not containing "yahoo.com"
+AND email NOT LIKE '%yahoo.com'; 
