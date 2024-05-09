f195a29fac3f07951e8476b699dba798ebebec93
ElvisMw <elvismwanthi@gmail.com>
2024-05-09 17:38:34 +0300

    2-fans.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,8 +1,6 @@
--- Selects the origin country and counts the number of fans for each country
-SELECT origin, COUNT(*) AS nb_fans
--- Specifies the table to select data from
+-- script that ranks country origins of bands,
+-- ordered by the number of (non-unique) fans
+SELECT origin, SUM(fans) as nb_fans
 FROM metal_bands
--- Groups the results by origin country
 GROUP BY origin
--- Orders the results by the number of fans in descending order
 ORDER BY nb_fans DESC;
