eac4357f76a03e737504a139d8373f0b4f6ec0dd
ElvisMw <elvismwanthi@gmail.com>
2024-05-17 20:47:01 +0300

    102-log_stats

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -2,37 +2,33 @@
 """
 Module for providing statistics about Nginx logs stored in MongoDB.
 """
-from pymongo import MongoClient
-from collections import Counter
+import pymongo
 
 
-def print_nginx_request_logs(nginx_collection):
+def print_nginx_stats(nginx_logs):
     """
     Prints stats about Nginx request logs.
 
     Args:
-        nginx_collection (pymongo.collection.Collection): The MongoDB
+        nginx_logs (pymongo.collection.Collection): The MongoDB
         collection object.
     """
-    # Count total logs
-    total_logs = nginx_collection.count_documents({})
+    total_logs = nginx_logs.count_documents({})
     print(f"{total_logs} logs")
 
-    # Count request methods
     print("Methods:")
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
     for method in methods:
-        req_count = nginx_collection.count_documents({'method': method})
-        print(f"\tmethod {method}: {req_count}")
+        method_count = nginx_logs.count_documents({'method': method})
+        print(f"{method}: {method_count}")
 
-    # Count status checks
-    status_checks_count = nginx_collection.count_documents(
+    print("Status Checks:")
+    status_checks_count = nginx_logs.count_documents(
         {'method': 'GET', 'path': '/status'}
     )
-    print(f"{status_checks_count} status check")
+    print(f"{status_checks_count} status checks")
 
-    # Count IP addresses
-    print("IPs:")
-    ips = Counter(doc['ip'] for doc in nginx_collection.find())
+    print("Top IPs:")
+    ips = pymongo.Counter(doc['ip'] for doc in nginx_logs.find())
     for ip, count in ips.most_common(10):
-        print(f"\t{ip}: {count}")
+        print(f"{ip}: {count}")
